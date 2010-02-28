662addc10b5106537d53c81c06992580d1babcb4
Dan Simpson <dan.simpson+git@gmail.com>
2010-02-27 17:04:48 -0800

    wav files

--- funnel_app/handlers/echo_handler.rb
+++ funnel_app/handlers/echo_handler.rb
@@ -1,25 +1,31 @@
+require "mq"
 
 class MusicGroup < EM::Channel
 
 	def initialize
 		super
+
 		@mq = MQ.new
-		@mq.queue.bind(mq.topic("amalgamaton"), :key => "#").subscribe do |msg|
+		@mq.queue("ws1").bind(@mq.topic("amalgamaton"), :key => "#").subscribe do |msg|
 			push msg
 		end
 	end
 
 end
 
-$group = MusicGroup.new
-
 class MusicHandler < Funnel::WebSocket::Connection
 
+	@@group = nil
+	
 	#called when the connection is ready to send
 	#and receive data
 	def on_ready
+		unless @@group
+			@@group = MusicGroup.new
+		end
+		
 		puts "Ready..."
-		@sid = $group.subscribe do |msg|
+		@sid = @@group.subscribe do |msg|
 			send_message msg
 		end
 	end
@@ -27,7 +33,7 @@ class MusicHandler < Funnel::WebSocket::Connection
 	#called on disconnect
 	def on_disconnect
 		puts "Ended"
-		$group.unsubscribe(@sid)
+		@@group.unsubscribe(@sid)
 	end
 	
 	#right back at you
