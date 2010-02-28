b6f5cdd3602dfbaed093d443b8a5055320c74de4
Nick Zadrozny <nick@beyondthepath.com>
2010-02-27 19:22:58 -0800

    Refactoring the monome stuff a bit, trying to add in some led lighting.

--- monome/monome.rb
+++ monome/monome.rb
@@ -4,6 +4,8 @@ require "eventmachine"
 require "amqp"
 require "mq"
 
+require "orchestra"
+
 BEATS_PER_MINUTE = 120
 
 mq = nil
@@ -15,47 +17,6 @@ EventMachine.run do
   mq.topic("amalgamaton").publish("hey there!", :key => "test")
   
   
-  class Orchestra
-
-    def initialize(mq)
-      @monome = MonomeSerial.detect_monome
-      @grid = Hash.new
-      @mq = mq
-    end
-
-    def keydown(x, y)
-      message = %({"action":"keydown","x":#{x},"y":#{y}})
-      puts message
-      @mq.topic("amalgamaton").publish(message)
-    end
-
-    def keyup(x, y)
-      message = %({"action":"keyup","x":#{x},"y":#{y}})
-      puts message
-      @mq.topic("amalgamaton").publish(message)
-    end
-
-    def conduct!
-      action, x, y = @monome.read
-      case action
-      when :keydown
-        keydown(x, y)
-      when :keyup
-        keyup(x, y)
-      else
-        puts "ACTION: #{action.inspect} (#{x}, #{y})"
-      end
-    end
-    
-    def tick!
-      @tick = ((@tick || 0) + 1) % 8
-      message = %({"action":"tick","offset":#{@tick}})
-      puts message
-      @mq.topic("amalgamaton").publish(message)
-    end
-
-  end
-
   orchestra = Orchestra.new(mq)
 
   Thread.new do
@@ -64,8 +25,8 @@ EventMachine.run do
     end
   end
 
-  # EM.add_periodic_timer(60.0 / BEATS_PER_MINUTE) do
-  #   orchestra.tick!
-  # end
+  EM.add_periodic_timer(60.0 / BEATS_PER_MINUTE) do
+    orchestra.tick!
+  end
   
 end
